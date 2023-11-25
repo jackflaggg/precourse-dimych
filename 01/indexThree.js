22ac3ffab3eb55407d598a1633445be99ffb2b42
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-25 20:29:58 +0500

    add lesson1

---
+++ 01/indexThree.js
@@ -0,0 +1,21 @@
+let card1 = {
+    type: 'debit',
+    isActive: true,
+    networkType: 'mastercard',
+    currencyType: 'USD',
+    balance: 5750.53,
+    number: 5282345678901289,
+    expirationYear: 2025,
+    expirationMonth: 9
+};
+
+let card2 = {
+    type: 'credit',
+    isActive: false,
+    networkType: 'visa',
+    currencyType: 'EUR',
+    balance: 1343.53,
+    number: 9232654321012032,
+    expirationYear: 2026,
+    expirationMonth: 4
+};
--- 01/indexTwo.js
+++ 01/indexTwo.js
@@ -11,7 +11,7 @@ let student = {
 
 student.address.city = 'Salavat';
 
-document.write('<b>firstName:</b> ', student.personalData.firstName, '<br>');
+document.write('<b>Personal DATA:</b> ', student.personalData.firstName, '<br>');
 document.write('<b>lastName:</b> ', student.personalData.lastName, '<br>');
 document.write('<b>country:</b> ', student.address.county, '<br>');
 document.write('<b>city:</b> ', student.address.city, '<br>');
