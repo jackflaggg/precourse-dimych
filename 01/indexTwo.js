dc163daa78efb90e439481deab0d68d268e70a10
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-25 20:00:52 +0500

    add lesson1

--- 01/indexTwo.js
+++ 01/indexTwo.js
@@ -5,6 +5,11 @@ let student = {
     },
     address: {
         county: 'Russia',
-        city: 'Paris',
+        city: 'UFA',
     }
-}
+};
+
+document.write('<b>firstName:</b> ', student.info.firstName, '<br>');
+document.write('<b>lastName:</b> ', student.info.lastName, '<br>');
+document.write('<b>country:</b> ', student.address.county, '<br>');
+document.write('<b>city:</b> ', student.address.city, '<br>');
