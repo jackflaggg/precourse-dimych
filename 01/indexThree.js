5fc810d9c242a8d2a36a097bc5f8696dcfb42693
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-25 21:09:32 +0500

    add lesson1

--- 01/indexThree.js
+++ 01/indexThree.js
@@ -24,26 +24,21 @@ let card2 = {
     expirationMonth: 4
 };
 
+let cards = [card1, card2];
+
 //render
 document.write('<h1>', 'Hello, ' + currentUser.name, '</h1>');
 document.write('<h2>', 'Cards: ', '</h2>');
 
-//render card 1
-document.write('<div>');
-document.write('<b>', card1.type + ' card', '</b>', '<br>');
-document.write('<b>', card1.networkType, '</b>', '<br>');
-document.write('<b>','current balance','</b>', '<br>');
-document.write('<span>', card1.currentBalance,'</span>', '<br>');
-document.write('<h4>', card1.number,'</h4>');
-document.write('<span>', card1.expirationMonth, '/', card1.expirationYear, '</span>');
-document.write('</div>');
+for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    document.write('<div>');
+    document.write('<b>', card.type + ' card', '</b>', '<br>');
+    document.write('<b>', card.networkType, '</b>', '<br>');
+    document.write('<b>','current balance','</b>', '<br>');
+    document.write('<span>', card.currentBalance,'</span>', '<br>');
+    document.write('<h4>', card.number,'</h4>');
+    document.write('<span>', card.expirationMonth, '/', card.expirationYear, '</span>');
+    document.write('</div>');
+}
 
-//render card 2
-document.write('<div>');
-document.write('<b>', card2.type + ' card', '</b>', '<br>');
-document.write('<b>', card2.networkType, '</b>', '<br>');
-document.write('<b>','current balance','</b>', '<br>');
-document.write('<span>', card2.currentBalance,'</span>', '<br>');
-document.write('<h4>', card2.number,'</h4>');
-document.write('<span>', card2.expirationMonth, '/', card2.expirationYear, '</span>');
-document.write('</div>');
