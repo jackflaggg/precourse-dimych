955c22702e273d1ffbd09bda229b4d2373333cb4
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-27 23:39:06 +0500

    add new value

--- 01/indexThree.js
+++ 01/indexThree.js
@@ -20,7 +20,7 @@ let card1 = {
         {
             title: 'Order Revenue',
             amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            date: new Date(2022, 3, 25, 16, 44, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
             title: 'Order Revenue',
