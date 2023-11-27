38909cdbc02eb775c9e18b65a0127c65ad6a7f70
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-28 00:28:14 +0500

    add new value

--- 01/indexThree.js
+++ 01/indexThree.js
@@ -13,56 +13,6 @@ for (let i = 0; i < cards.length; i++) {
 
 }
 
-for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
-    document.write('<div class="card-block">');
-    document.write('<b>', card.type + ' card', '</b>', '<br>');
-
-    let networkLogo = '';
-    switch (card.networkType) {
-        case 'visa':
-            networkLogo = '<img src="/png/1.png">';
-            break;
-
-        case 'mastercard':
-            networkLogo = '<img src="/png/3.png">';
-            break;
-    }
-
-    document.write(networkLogo);
-    document.write('<b>','current balance','</b>', '<br>');
-
-    if (card.currencyType === 'USD') {
-        document.write('<span>', '$ ' + card.currentBalance,'</span>', '<br>');
-    } else if (card.currencyType === 'EUR'){
-        document.write('<span>', '€ ' + card.currentBalance,'</span>', '<br>');
-    } else {
-        document.write('<span>', '£ ' + card.currentBalance, '</span>', '<br>');
-    }
-    document.write('<h4>', card.number,'</h4>');
-    document.write('<span>', card.expirationMonth, '/', card.expirationYear, '</span>');
-    document.write('</div>');
-    document.write("<h2>", "History Transaction", "</h2>");
-    document.write("<ul>");
-    for (let j = 0; j < card.transactions.length; j++) {
-        let transaction = card.transactions[j];
-        document.write("<li>", transaction.title, ", ", transaction.date, ",");
-
-        //условный рендеринг
-        if (transaction.amount > 0) {
-            document.write('<span class="income">', transaction.amount, '</span>');
-        } else{
-            document.write('<span class="outcome">', transaction.amount, '</span>');
-        }
-        document.write("</li>")
-    }
-
-    document.write("</ul>");
-    document.write('</div>');
-    document.write('<hr>');
-}
-
-
 function getCurrentUser(){
     //функция создает и возвращает объект
     let newUser = {
@@ -168,5 +118,49 @@ function renderHeader(userName){
 }
 
 function renderCard(card){
+    document.write('<div class="card-block">');
+    document.write('<b>', card.type + ' card', '</b>', '<br>');
 
+    let networkLogo = '';
+    switch (card.networkType) {
+        case 'visa':
+            networkLogo = '<img src="/png/1.png">';
+            break;
+
+        case 'mastercard':
+            networkLogo = '<img src="/png/3.png">';
+            break;
+    }
+
+    document.write(networkLogo);
+    document.write('<b>','current balance','</b>', '<br>');
+
+    if (card.currencyType === 'USD') {
+        document.write('<span>', '$ ' + card.currentBalance,'</span>', '<br>');
+    } else if (card.currencyType === 'EUR'){
+        document.write('<span>', '€ ' + card.currentBalance,'</span>', '<br>');
+    } else {
+        document.write('<span>', '£ ' + card.currentBalance, '</span>', '<br>');
+    }
+    document.write('<h4>', card.number,'</h4>');
+    document.write('<span>', card.expirationMonth, '/', card.expirationYear, '</span>');
+    document.write('</div>');
+    document.write("<h2>", "History Transaction", "</h2>");
+    document.write("<ul>");
+    for (let j = 0; j < card.transactions.length; j++) {
+        let transaction = card.transactions[j];
+        document.write("<li>", transaction.title, ", ", transaction.date, ",");
+
+        //условный рендеринг
+        if (transaction.amount > 0) {
+            document.write('<span class="income">', transaction.amount, '</span>');
+        } else{
+            document.write('<span class="outcome">', transaction.amount, '</span>');
+        }
+        document.write("</li>")
+    }
+
+    document.write("</ul>");
+    document.write('</div>');
+    document.write('<hr>');
 }
